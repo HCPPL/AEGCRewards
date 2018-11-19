1205184ff5111dbdb5bf13d6a4f4c3d9b4834888
Gagandeep Kaur <44575923+GaganHC@users.noreply.github.com>
2018-11-19 19:04:32 +0530

    new updates

--- DevelopmentAcc.sol
+++ DevelopmentAcc.sol
@@ -1,9 +1,11 @@
 pragma solidity ^0.4.24;
 
+import "./contracts/ownership/Ownable.sol";
 import "./AegisEconomyCoin.sol";
 import "./contracts/math/SafeMath.sol";
 
-contract DevelopmentAcc {	
+
+contract DevelopmentAcc is Ownable {	
 
  	AegisEconomyCoin public aegisCoin;
 
@@ -13,6 +15,12 @@ contract DevelopmentAcc {
  	uint256 public firstWinnerPercentage; 
  	uint256 public secondWinnerPercentage; 
  	uint256 public thirdWinnerPercentage; 
+
+ 	/// Modifier to allow only aegisCoin contract to call a method
+ 	modifier onlyAdmin() {
+ 		require (msg.sender == address(aegisCoin));
+ 		_;
+  	} 
  	
 	/// @author Gagandeep_HashCode
     /// @notice Contructor for initial setup
@@ -25,22 +33,66 @@ contract DevelopmentAcc {
 	{
 			require(_address != address(0));
 			aegisCoin = _address;
-			aegisCoin.setDevelopmentAcc(address(this));
-
-			updateDeveloperVoterPercentage(_devPercentage, _voterPercentage);
-			updateWinnersPercentage(_firstWinnerPer, _secondWinnerPer, _thirdWinnerPer);
+			
+			setDeveloperVoterPercentage(_devPercentage, _voterPercentage);
+			setWinnersPercentage(_firstWinnerPer, _secondWinnerPer, _thirdWinnerPer);
 	}	
+
+
+    function setDevelopmentAccountForAegisCoin() 
+	onlyOwner
+	public
+	{
+			aegisCoin.setDevelopmentAcc(address(this));
+	}
+
+	/// @notice Function to transfer development funds to any address
+	/// @param _receiver Receiver's address
+	/// @param _value Amount of tokens to be sent
+	function transferTokens(address _receiver, uint256 _value) 
+	onlyAdmin 
+	public 
+	{
+			aegisCoin.transfer(_receiver, _value);
+	}
 
 
 	// Set figures for developer and reviewers
 	/// @notice Function to update distributive percentage figures for developers and voters
 	/// @param _devPercentage Percentage value for developers
 	/// @param _voterPercentage Percentage value for voters
-	function updateDeveloperVoterPercentage(uint256 _devPercentage, uint256 _voterPercentage) 
-	public 
+	function setDeveloperVoterPercentage(uint256 _devPercentage, uint256 _voterPercentage) 
+	private 
 	{
 			developersFundPercentage = _devPercentage;
 			votersFundPercentage = _voterPercentage;
+	}
+
+
+	// Set figures for first, second and third winners
+	/// @notice Function to update distributive percentage figures for first, second and third winners
+	/// @param _firstWinnerPer Percentage value for first winner
+	/// @param _secondWinnerPer Percentage value for second winner
+	/// @param _thirdWinnerPer Percentage value for third winner
+	function setWinnersPercentage(uint256 _firstWinnerPer,
+	    uint256 _secondWinnerPer,
+	    uint256 _thirdWinnerPer) 
+	private 
+	{
+			firstWinnerPercentage = _firstWinnerPer;
+			secondWinnerPercentage = _secondWinnerPer;
+			thirdWinnerPercentage = _thirdWinnerPer;
+	}
+
+	// Set figures for developer and reviewers
+	/// @notice Wrapper Function to update distributive percentage figures for developers and voters
+	/// @param _devPercentage Percentage value for developers
+	/// @param _voterPercentage Percentage value for voters
+	function updateDeveloperVoterPercentage(uint256 _devPercentage, uint256 _voterPercentage) 
+	public 
+	{
+        	// require ( _devPercentage.add(_voterPercentage) == 100); 
+			setDeveloperVoterPercentage(_devPercentage, _voterPercentage);
 	}
 
 
@@ -54,9 +106,8 @@ contract DevelopmentAcc {
 	    uint256 _thirdWinnerPer) 
 	public 
 	{
-			firstWinnerPercentage = _firstWinnerPer;
-			secondWinnerPercentage = _secondWinnerPer;
-			thirdWinnerPercentage = _thirdWinnerPer;
+            // require ( _firstWinnerPer.add(_secondWinnerPer.add(_thirdWinnerPer)) == 100); 
+			updateWinnersPercentage(_firstWinnerPer, _secondWinnerPer, _thirdWinnerPer);
 	}
 
 
@@ -65,51 +116,51 @@ contract DevelopmentAcc {
 	// }
 
 
-	// Suggestions function to keep track of price winners
-	// for that mapping will be there and this method will be called (and will throw an event) whenever transactions are made to winners and reviewers
-	function releaseTokens(address _firstWinner, 
-		address _secondWinner, 
-		address _thirdWinner, 
-		uint256 _backlogTokenAmount,
-		uint256 _totalVoters)			// param: backlog-id - map backlog-id with totalVoters/TokensPerVoter
-	public 
-	{
-			// param: reviewers, first/second/third winner address and unique-id
-			// call releaseTokensForCompleteBackLog() method
-			uint256 tokensForDevelopers;
-			uint256 tokensForVoters;
+	// // Suggestions function to keep track of price winners
+	// // for that mapping will be there and this method will be called (and will throw an event) whenever transactions are made to winners and reviewers
+	// function releaseTokens(address _firstWinner, 
+	// 	address _secondWinner, 
+	// 	address _thirdWinner, 
+	// 	uint256 _backlogTokenAmount,
+	// 	uint256 _totalVoters)			// param: backlog-id - map backlog-id with totalVoters/TokensPerVoter
+	// public 
+	// {
+	// 		// param: reviewers, first/second/third winner address and unique-id
+	// 		// call releaseTokensForCompleteBackLog() method
+	// 		uint256 tokensForDevelopers;
+	// 		uint256 tokensForVoters;
 
-			uint256 tokensForFirstWinner;
-			uint256 tokensForSecondWinner;
-			uint256 tokensForThirdWinner;
+	// 		uint256 tokensForFirstWinner;
+	// 		uint256 tokensForSecondWinner;
+	// 		uint256 tokensForThirdWinner;
 
-			(tokensForDevelopers, tokensForVoters) = calculateFunds(_backlogTokenAmount);
+	// 		(tokensForDevelopers, tokensForVoters) = calculateFunds(_backlogTokenAmount);
 
-			(tokensForFirstWinner, tokensForSecondWinner, tokensForThirdWinner) = calculateWinnerFunds(tokensForDevelopers);
+	// 		(tokensForFirstWinner, tokensForSecondWinner, tokensForThirdWinner) = calculateWinnerFunds(tokensForDevelopers);
 
-			uint256 tokensPerVoter = calculateVoterFunds(tokensForVoters, _totalVoters);
+	// 		uint256 tokensPerVoter = calculateVoterFunds(tokensForVoters, _totalVoters);
 
-			// do token amount check with reserved value and not with owner's balance before sending it to transfer function
+	// 		// do token amount check with reserved value and not with owner's balance before sending it to transfer function
 
-			aegisCoin.transfer(_firstWinner, tokensForFirstWinner);
-			aegisCoin.transfer(_secondWinner, tokensForSecondWinner);
-			aegisCoin.transfer(_thirdWinner, tokensForThirdWinner);
+	// 		aegisCoin.transfer(_firstWinner, tokensForFirstWinner);
+	// 		aegisCoin.transfer(_secondWinner, tokensForSecondWinner);
+	// 		aegisCoin.transfer(_thirdWinner, tokensForThirdWinner);
 
-			// TODO: transfer funds to all voters
-	} 
+	// 		// TODO: transfer funds to all voters
+	// } 
 
-	/// @notice Function to calculate token value for developers and voters as per the backlog amount given
-	/// @param _backlogAmount Amount of token assigned to the given backlog
-	/// @return _tokensForDev Amount of tokens reserved for developers
-	/// @return _tokensForVoters Amount of tokens reserved for voters
-	function calculateFunds(uint256 _backlogAmount)
-	private
-	returns (uint256 _tokensForDev, uint256 _tokensForVoters) 
-	{
-			_tokensForDev = (_backlogAmount.mul(developersFundPercentage)).div(100);
-			_tokensForVoters = (_backlogAmount.mul(votersFundPercentage)).div(100);
-			return;
-	}
+	// /// @notice Function to calculate token value for developers and voters as per the backlog amount given
+	// /// @param _backlogAmount Amount of token assigned to the given backlog
+	// /// @return _tokensForDev Amount of tokens reserved for developers
+	// /// @return _tokensForVoters Amount of tokens reserved for voters
+	// function calculateFunds(uint256 _backlogAmount)
+	// private
+	// returns (uint256 _tokensForDev, uint256 _tokensForVoters) 
+	// {
+	// 		_tokensForDev = (_backlogAmount.mul(developersFundPercentage)).div(100);
+	// 		_tokensForVoters = (_backlogAmount.mul(votersFundPercentage)).div(100);
+	// 		return;
+	// }
 
 
 	// function calculateWinnerFunds(uint256 _backlogAmountForDevelopers) 
@@ -250,5 +301,5 @@ contract DevelopmentAcc {
  			return(firstWinnerPercentage, secondWinnerPercentage, thirdWinnerPercentage); 
     }
 
-    // Our service to check if gas is exhausted or not. There should be enough ethers to execute all the method calls inline.
+    // Our service to check if gas is exhausted or not. There should be enough ethers to execute all the method calls that are inline.
 }
